72068663d310b146281a466ea663ce22ed566c06
fujiiakina-13640 <fujii.akina_1403@outlook.jp>
2025-12-19 23:37:19 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+ <title>PHP基礎kadai_007連想配列</title>
+</head>
+
+<body>
+
+  <p>
+    
+    <?php
+    $commodity = ['name' => 'onion','price' => 200,'weight' => 160,];
+    echo '<br>';
+    print_r($commodity);
+    ?>
+
+  </p>
+
+</body>
+
+</html>
